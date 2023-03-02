e70f2ae4f45b8f4a190d0cde665dcff848706409
ManhLinh2597 <linh.vm25@gmail.com>
2023-03-02 09:20:30 +0700

    example callback function

--- scope.js
+++ scope.js
@@ -1,4 +1,28 @@
-const arr = [9,255,63,100,9999,100000]
-arr.sort((item1,item2) => item2 - item1                  )
-console.log(arr)
+const callback = (err, data) => {
+    if (err){
+        console.log(">>> callback: ", err)
+    }
+    if (data){
+        console.log(">> callback: ", data)
+    }
+}
+
+function getTodos(){
+    var request = new XMLHttpRequest()
+    
+    request.onreadystatechange = function () {
+        if (this.readyState === 4 && request.status === 200){
+            const data = request.responseText
+            callback(undefined, data)
+        }
+        if (this.readyState === 4 && request.status !== 200){
+            
+            callback("Something wrong", undefined)
+        }
+    }
+    request.open("GET","https://jsonplaceholder.typicode.com/todos", true)
+    request.send()
+}
+
+getTodos(callback)
 
